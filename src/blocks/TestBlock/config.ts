a5e16d0fafba9a800ebd10751dd9fd37cc1268b9
slavasolutions <134661922+slavasolutions@users.noreply.github.com>
2025-05-07 11:32:33 -0400

    Create config.ts

---
+++ src/blocks/TestBlock/config.ts
@@ -0,0 +1,12 @@
+import { Block } from 'payload/types';
+
+export const sample-cardSchema: Block = {
+  slug: 'sample-card',
+  labels: {
+    singular: 'SampleCard',
+    plural: 'SampleCards',
+  },
+  fields: [
+    
+  ],
+};
